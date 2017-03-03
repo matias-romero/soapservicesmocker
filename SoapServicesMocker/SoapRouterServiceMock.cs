657357133709e1fa01702f99b8ec5a4c32e32ac2
matias-romero <matias@eymcomputers.com.ar>
2017-03-03 15:41:13 -0300

    Project Layout

---
+++ SoapServicesMocker/SoapRouterServiceMock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SoapServicesMocker
+{
+    public class SoapRouterServiceMock : IDisposable
+    {
+        private readonly Uri _testRunnerServerUri;
+        private UnitaryWebServer _loopbackHttpServer;
+        private IDictionary<string, Tuple<Func<string, bool>, Action<HttpListenerResponse>>> _soapActionRules = new Dictionary<string, Tuple<Func<string, bool>, Action<HttpListenerResponse>>>();
+
+        public SoapRouterServiceMock(string webServiceAppEndpoint)
+        {
+            if (!webServiceAppEndpoint.StartsWith("/"))
+                webServiceAppEndpoint = "/" + webServiceAppEndpoint;
+            if (!webServiceAppEndpoint.EndsWith("/"))
+                webServiceAppEndpoint = webServiceAppEndpoint + "/";
+
+            _testRunnerServerUri = UnitaryWebServer.ResolveLocalRandomPrefix(webServiceAppEndpoint);
+            _loopbackHttpServer = new UnitaryWebServer(_testRunnerServerUri, this.RouteResponse);
+            _loopbackHttpServer.Run();
+        }
+
+        public Uri WebServiceEndpoint
+        {
+            get { return _testRunnerServerUri; }
+        }
+
+        private void SendSoapXmlResponse(HttpListenerResponse response, string Xml)
+        {
+            response.ContentEncoding = Encoding.UTF8;
+            response.ContentType = "text/xml";
+            //response.SetCookie(new Cookie("AuthenticationCookie", "abcabc"));
+            var bytes = Encoding.UTF8.GetBytes(Xml);
+            response.Close(bytes, true);
+        }
+
+        public void Dispose()
+        {
+            if (_loopbackHttpServer != null)
+            {
+                _loopbackHttpServer.Stop();
+                _loopbackHttpServer.Dispose();
+                _loopbackHttpServer = null;
+            }
+        }
+
+        private void RouteResponse(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            var soapAction = request.Headers["SOAPAction"];
+            var receivedEntity = new StreamReader(request.InputStream, request.ContentEncoding).ReadToEnd();
+
+            if (_soapActionRules.ContainsKey(soapAction))
+            {
+                var tuple = _soapActionRules[soapAction];
+                if (tuple.Item1.Invoke(receivedEntity))
+                    tuple.Item2.Invoke(response);
+            }
+        }
+
+        public void ConfigureResponseForSOAPAction(string fullyQualifiedSoapAction, string xmlResponse)
+        {
+            _soapActionRules[fullyQualifiedSoapAction] = Tuple.Create<Func<string, bool>, Action<HttpListenerResponse>>(AlwaysPredicate, (res) => SendSoapXmlResponse(res, xmlResponse));
+        }
+
+        private bool AlwaysPredicate(string entityBody) { return true; }
+    }
+}
---
+++ SoapServicesMocker/UnitaryWebServer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SoapServicesMocker
+{
+    /// <summary>
+    /// Creates a mini web-server capable of handle Request-Response flows in localhost
+    /// </summary>
+    public class UnitaryWebServer : IDisposable
+    {
+        private bool _disposed;
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Action<HttpListenerRequest, HttpListenerResponse> _responderMethod;
+        
+        public UnitaryWebServer(string[] prefixes, Action<HttpListenerRequest, HttpListenerResponse> method)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public UnitaryWebServer(Action<HttpListenerRequest, HttpListenerResponse> method, params string[] prefixes)
+            : this(prefixes, method)
+        {
+        }
+
+        public UnitaryWebServer(Uri registeredUri, Action<HttpListenerRequest, HttpListenerResponse> method)
+            : this(method, registeredUri.ToString())
+        {
+        }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Debug.Print("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                _responderMethod.Invoke(ctx.Request, ctx.Response);
+                            }
+                            catch { } // suppress any exceptions
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+            _disposed = true;
+            Debug.Print("Webserver stopped...");
+        }
+
+        public void Dispose()
+        {
+            if(!_disposed)
+                this.Stop();
+        }
+
+        private static int FreeTcpPort()
+        {
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            var port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+
+        public static Uri ResolveLocalRandomPrefix(string absolutePath)
+        {
+            var urlTemplate = string.Format("http://localhost:{0}/{1}/", FreeTcpPort(), absolutePath.Trim('/'));
+            return new Uri(urlTemplate, UriKind.Absolute);
+        }
+    }
+}
